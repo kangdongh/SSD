dc2ef1da7205abd90cb3c73d3ee1dea14570cdd2
jeonghun <jhe.kim@samsung.com>
2024-07-16 08:42:47 +0900

    Revert "[feat] Modify Help, Exit method"
    This reverts commit 9adf9b62c36b00ca1c668e0df3fb33f64e4e6456.

--- app/basic_logic.py
+++ app/basic_logic.py
@@ -1,5 +1,3 @@
-import os
-
 class BasicLogic:
     _ssd_path: str
 
@@ -7,52 +5,35 @@ class BasicLogic:
         self._ssd_path = ssd_path
 
     def read(self, args):
-        # Arg 1 : LBA 정보
-        arg1 = 2
-        result = 0x9988FFFF  # LBA 2의 정보 출력
-        print(f'Read Value from LBA {arg1}')
+        pass
 
-        if os.path.exists(self._ssd_path):
-            os.remove(self._ssd_path)
-        with open('result.txt', 'w') as file:  # _ssd_path = result.txt
-            file.write('\n')
-
-    def write(self, *args):
-        # Arg 1 : LBA 정보
-        # Arg 2 : 저장할 값
-        arg1 = 3
-        arg2 = 0x9988FFFF
-        print(f'Write Value {arg2} to LBA {arg1}')
+    def write(self, args):
+        pass
 
     def full_read(self):
-        for lba in range(0, 99 + 1):
-            self.read(lba)
+        pass
 
     def full_write(self):
-        print(f'Start to write value at all LBAs')
-        value = 0x00000000
-        for lba in range(0, 99 + 1):
-            self.write(lba, value)
+        pass
 
     def help(self) -> str:
         help_str = """
-****************************************
-********** COMMAND HELP **********
-****************************************
-1. ** write: Write value to SSD / usage: write <LBA> <VALUE>
-2. ** read: Read value from SSD / usage: read <LBA>
-3. ** exit: Exit program / usage exit
-4. ** help: Help command
-5. ** fullwrite: Write value at all LBAs / usage: fullwrite <VALUE>
-6. ** fullread: Read all LBAs / usage: fullread
-7. ** testapp1: After fullwrite, Check If fullread value is correct according to the written value / usage: testapp1
-8. ** testapp2: After overwrite, read the LBA value to make sure LBA value is correct according to overwritten value / usage: testapp2
-****************************************
-        """
+        ****************************************
+        ********** COMMAND HELP **********
+        ****************************************
+        1. ** write: Write value to SSD / usage: write <LBA> <VALUE>
+        2. ** read: Read value from SSD / usage: read <LBA>
+        3. ** exit: Exit program / usage exit
+        4. ** help: Help command
+        5. ** fullwrite: Write value at all LBAs / usage: fullwrite <VALUE>
+        6. ** fullread: Read all LBAs / usage: fullread
+        7. ** testapp1: After fullwrite, Check If fullread value is correct according to the written value / usage: testapp1
+        8. ** testapp2: After overwrite, read the LBA value to make sure LBA value is correct according to overwritten value / usage: testapp2
+        ****************************************
+                """
 
         print(f'{help_str}')
         return help_str
 
-    def exit(self) -> bool:
-        print(f'Exit SSD Program')
-        return True
+    def exit(self) -> str:
+        pass
--- tests/test_basic_logic.py
+++ tests/test_basic_logic.py
@@ -1,14 +1,7 @@
-import unittest
 from unittest import TestCase, skip
-
-from app.basic_logic import BasicLogic
 
 
 class TestBasicLogic(TestCase):
-
-    def setUp(self):
-        self.sut = BasicLogic("test_basic_logic")
-
     def test_read(self):
         pass
 
@@ -22,19 +15,13 @@ class TestBasicLogic(TestCase):
         pass
 
     def test_exit(self):
-        self.assertEqual(self.sut.exit(), True)
+        pass
 
     def test_help(self):
-        self.assertIsNotNone(self.sut.help())
+        pass
 
-    @skip
     def test_test_app1(self):
         pass
 
-    @skip
     def test_test_app2(self):
         pass
-
-
-if __name__ == '__main__':
-    unittest.main()
