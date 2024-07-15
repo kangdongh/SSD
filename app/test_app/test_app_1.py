41620ca86f400083c024ce342a63ea86c6af3fbd
jihun75.kim <gnswlrla0218@naver.com>
2024-07-15 16:32:13 +0900

    [feature] fix TC for test_app and update TestApp1 and 2

--- app/test_app/test_app_1.py
+++ app/test_app/test_app_1.py
@@ -6,4 +6,9 @@ class TestApp1(ITestApp):
     FULL_WRITE_VALUE = '0x00000001'
 
     def run(self, basic_logic: BasicLogic):
-        pass
+        basic_logic.full_write(TestApp1.FULL_WRITE_VALUE)
+        results = basic_logic.full_read().strip().split('\n')
+        for result in results:
+            if result.lower() != TestApp1.FULL_WRITE_VALUE.lower():
+                raise RuntimeError(f"{self.__class__.__name__} Failure")
+        print(f"{self.__class__.__name__} PASS")
--- app/test_app/test_app_2.py
+++ app/test_app/test_app_2.py
@@ -8,4 +8,18 @@ class TestApp2(ITestApp):
     OVERWRITE_VALUE = '0x12345678'
 
     def run(self, basic_logic: BasicLogic):
-        pass
+        for j in range(TestApp2.DUPL_WRITE_COUNT):
+            for i in range(0, 6):
+                basic_logic.write(str(i), TestApp2.FIRST_WRITE_VALUE)
+
+        for i in range(0, 6):
+            basic_logic.write(str(i), TestApp2.OVERWRITE_VALUE)
+
+        for i in range(0, 6):
+            read = basic_logic.read(str(i))
+            print(read)
+            if read != TestApp2.OVERWRITE_VALUE:
+                print(f"{self.__class__.__name__} Failure, read {read} vs Expected {TestApp2.OVERWRITE_VALUE}")
+                raise RuntimeError()
+
+        print(f"{self.__class__.__name__} PASS")
