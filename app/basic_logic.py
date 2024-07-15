0f26eff34172b8abfd2e0d83dba2ec7b0314e54b
jeonghun <jhe.kim@samsung.com>
2024-07-15 16:05:37 +0900

    [feat] Add Help Command

--- app/basic_logic.py
+++ app/basic_logic.py
@@ -17,7 +17,23 @@ class BasicLogic:
         pass
 
     def help(self) -> str:
-        pass
+        help_str = """
+        ****************************************
+        ********** COMMAND HELP **********
+        ****************************************
+        1. ** write: Write value to SSD / usage: write <LBA> <VALUE>
+        2. ** read: Read value from SSD / usage: read <LBA>
+        3. ** exit: Exit program / usage exit
+        4. ** help: Help command
+        5. ** fullwrite: Write value at all LBAs / usage: fullwrite <VALUE>
+        6. ** fullread: Read all LBAs / usage: fullread
+        7. ** testapp1: After fullwrite, Check If fullread value is correct according to the written value / usage: testapp1
+        8. ** testapp2: After overwrite, read the LBA value to make sure LBA value is correct according to overwritten value / usage: testapp2
+        ****************************************
+                """
+
+        print(f'{help_str}')
+        return help_str
 
     def exit(self) -> str:
         pass
