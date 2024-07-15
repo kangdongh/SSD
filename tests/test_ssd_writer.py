78e15ef2937fd39deb6881bcc0c01f8af101280d
JoBD <cbd902@naver.com>
2024-07-16 08:41:55 +0900

    [fix] ssd_write기능 테스트 케이스 수정

--- tests/test_ssd_writer.py
+++ tests/test_ssd_writer.py
@@ -1,34 +1,44 @@
+import os
 from unittest import TestCase, skip
-from unittest.mock import Mock
 from hardware.ssd_writer import SSDWriter
 
+WRITE_FILE_NAME = './nand.txt'
+INIT_VALUE = '0x00000000\n'
 class TestSSDWriter(TestCase):
 
-    @skip
+    def setUp(self):
+        super().setUp()
+        self.ssd_writer = SSDWriter()
+        self.file_handler = open(WRITE_FILE_NAME, 'w')
+        self.file_handler.write(INIT_VALUE)
+        self.file_handler.write(INIT_VALUE)
+        self.file_handler.write(INIT_VALUE)
+        self.file_handler.close()
+
+    def tearDown(self):
+        os.remove(WRITE_FILE_NAME)
+    #@skip
     def test_write01(self):
-        write_file_name = 'nand.txt'
         logical_bytes_address = 1
         data_to_write = '0x9988FFFA'
 
-        ssd_writer = SSDWriter()
-        ssd_writer.write(write_file_name, logical_bytes_address, data_to_write)
-        self.assertEqual(data_to_write, self.read_from_line(write_file_name, logical_bytes_address))
+        self.ssd_writer.write(WRITE_FILE_NAME, logical_bytes_address, data_to_write)
+        self.assertEqual(data_to_write, self.read_from_line(WRITE_FILE_NAME, logical_bytes_address))
 
     @skip
     def test_write02(self):
-        write_file_name = 'nand.txt'
         logical_bytes_address = 2
         data_to_write = '0x9988FFFB'
 
-        ssd_writer = SSDWriter()
-        ssd_writer.write(write_file_name, logical_bytes_address, data_to_write)
-        self.assertEqual(data_to_write, self.read_from_line(write_file_name, logical_bytes_address))
+        self.ssd_writer.write(WRITE_FILE_NAME, logical_bytes_address, data_to_write)
+        self.assertEqual(data_to_write, self.read_from_line(WRITE_FILE_NAME, logical_bytes_address))
 
-    def read_from_line(self, write_file_name, logical_bytes_address):
-        open_file = open(write_file_name, 'r')
+    def read_from_line(self, read_file_name, logical_bytes_address) -> str:
+        file_handler = open(read_file_name, 'r')
+
         line_content = None
-
-        for current_line_number, line in enumerate(open_file, start=0):
+        for current_line_number, line in enumerate(file_handler, start=0):
             if current_line_number == logical_bytes_address:
                 line_content = line.strip()
+                file_handler.close()
                 return line_content
