9adf9b62c36b00ca1c668e0df3fb33f64e4e6456
jeonghun <jhe.kim@samsung.com>
2024-07-15 16:45:36 +0900

    [feat] Modify Help, Exit method

--- tests/test_basic_logic.py
+++ tests/test_basic_logic.py
@@ -1,7 +1,14 @@
+import unittest
 from unittest import TestCase, skip
+
+from app.basic_logic import BasicLogic
 
 
 class TestBasicLogic(TestCase):
+
+    def setUp(self):
+        self.sut = BasicLogic("test_basic_logic")
+
     def test_read(self):
         pass
 
@@ -15,13 +22,19 @@ class TestBasicLogic(TestCase):
         pass
 
     def test_exit(self):
-        pass
+        self.assertEqual(self.sut.exit(), True)
 
     def test_help(self):
-        pass
+        self.assertIsNotNone(self.sut.help())
 
+    @skip
     def test_test_app1(self):
         pass
 
+    @skip
     def test_test_app2(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
