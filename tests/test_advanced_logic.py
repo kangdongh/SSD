6db5fb4ef8908f2a7b1e48065705145c004d2c53
jeff.kang <jeff.kang@samsung.com>
2024-07-15 13:32:56 +0900

    [feat] add interfaces for testing

---
+++ tests/test_advanced_logic.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestAdvancedLogic(TestCase):
+    def test_test_app1(self):
+        self.fail()
+
+    def test_test_app2(self):
+        self.fail()
---
+++ tests/test_ssd_reader.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+
+class TestSSDReader(TestCase):
+    pass
