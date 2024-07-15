d96d0247674631ec3ae4fa64dede2b7283ca5bca
jeff.kang <jeff.kang@samsung.com>
2024-07-15 14:41:41 +0900

    [feat] modify basic logic interface

--- app/basic_logic.py
+++ app/basic_logic.py
@@ -1,5 +1,9 @@
-
 class BasicLogic:
+    _ssd_path: str
+
+    def __init__(self, ssd_path):
+        self._ssd_path = ssd_path
+
     def read(self, args):
         pass
 
@@ -17,4 +21,3 @@ class BasicLogic:
 
     def exit(self) -> str:
         pass
-
--- app/test_shell.py
+++ app/test_shell.py
@@ -9,16 +9,10 @@ class TestShell:
     _test_app1: ITestApp
     _test_app2: ITestApp
 
-    def __init__(self, logic: BasicLogic):
+    def __init__(self, logic: BasicLogic, test_app1: ITestApp, test_app2: ITestApp):
         self._logic = logic
-        self.set_test_app1(TestApp1())
-        self.set_test_app2(TestApp2())
-
-    def set_test_app2(self, test_app_2):
-        self._test_app2 = test_app_2
-
-    def set_test_app1(self, test_app_1):
-        self._test_app1 = test_app_1
+        self._test_app1 = test_app1
+        self._test_app2 = test_app2
 
     def run(self, line) -> bool:
         # Call _app.methods
@@ -27,8 +21,12 @@ class TestShell:
 
 
 if __name__ == '__main__':
-    logic = BasicLogic()
-    app = TestShell(logic)
+    import os.path
+
+    current_file_abspath = os.path.abspath(__file__)
+    ssd_path = os.path.join(current_file_abspath, '../hardware/ssd.py')
+    basic_logic = BasicLogic(ssd_path)
+    app = TestShell(basic_logic, TestApp1(), TestApp2())
     while True:
         inp = input()
         exit_condition = app.run(inp)
