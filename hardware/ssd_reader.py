6d21011779ec66c0ff9e29ed3c09144a7bd2ea65
junhyeok-park <parkerk4@naver.com>
2024-07-15 15:44:29 +0900

    [feat] add test functions for ssd reader

--- hardware/ssd_reader.py
+++ hardware/ssd_reader.py
@@ -2,4 +2,9 @@ from hardware.ssd_reader_interface import ISSDReader
 
 
 class SSDReader(ISSDReader):
-    pass
+    def read(
+            self,
+            read_file_name,
+            logical_bytes_address: int
+    ) -> str:
+        return ''
--- tests/test_ssd_reader.py
+++ tests/test_ssd_reader.py
@@ -1,6 +1,32 @@
-from unittest import TestCase
+import os
+from random import randint
+from unittest import TestCase, skip
+
+from hardware.ssd_reader import SSDReader
+
+READ_FILE_DIR = './nand_test.txt'
+VALUE_FIRST = '0xAAAABBBB\n'
+VALUE_SECOND = '0x00001111\n'
 
 
 class TestSSDReader(TestCase):
+    def setUp(self):
+        self.file_handler = open(READ_FILE_DIR, 'w')
+        for _ in range(50):
+            self.file_handler.write(VALUE_FIRST)
+        for _ in range(50):
+            self.file_handler.write(VALUE_SECOND)
+        self.file_handler.close()
+
+    def tearDown(self):
+        os.remove(READ_FILE_DIR)
+
+    @skip
     def test_read(self):
-        pass
+        ssd_reader = SSDReader()
+        target_address = randint(0, 99)
+        answer = VALUE_FIRST if target_address < 50 else VALUE_SECOND
+
+        ret = ssd_reader.read(READ_FILE_DIR, target_address)
+
+        self.assertEqual('0xAAAABBBB', ret)
