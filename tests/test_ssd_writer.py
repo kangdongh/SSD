c9a7a8ae40c0a0f452bf45632bd35c63de9bb30a
JoBD <cbd902@naver.com>
2024-07-15 16:39:48 +0900

    add ssd_writer test function

--- tests/test_ssd_writer.py
+++ tests/test_ssd_writer.py
@@ -1,6 +1,34 @@
-from unittest import TestCase
-
+from unittest import TestCase, skip
+from unittest.mock import Mock
+from hardware.ssd_writer import SSDWriter
 
 class TestSSDWriter(TestCase):
-    def test_write(self):
-        pass
+
+    @skip
+    def test_write01(self):
+        write_file_name = 'nand.txt'
+        logical_bytes_address = 1
+        data_to_write = '0x9988FFFA'
+
+        ssd_writer = SSDWriter()
+        ssd_writer.write(write_file_name, logical_bytes_address, data_to_write)
+        self.assertEqual(data_to_write, self.read_from_line(write_file_name, logical_bytes_address))
+
+    @skip
+    def test_write02(self):
+        write_file_name = 'nand.txt'
+        logical_bytes_address = 2
+        data_to_write = '0x9988FFFB'
+
+        ssd_writer = SSDWriter()
+        ssd_writer.write(write_file_name, logical_bytes_address, data_to_write)
+        self.assertEqual(data_to_write, self.read_from_line(write_file_name, logical_bytes_address))
+
+    def read_from_line(self, write_file_name, logical_bytes_address):
+        open_file = open(write_file_name, 'r')
+        line_content = None
+
+        for current_line_number, line in enumerate(open_file, start=0):
+            if current_line_number == logical_bytes_address:
+                line_content = line.strip()
+                return line_content
