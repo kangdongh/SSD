9d2ea9c84edae9a74d8a6fb79d92c8317500f989
jeonghun <jhe.kim@samsung.com>
2024-07-16 10:07:11 +0900

    [feat] Update Help method

--- app/basic_logic.py
+++ app/basic_logic.py
@@ -27,13 +27,8 @@ class BasicLogic:
         4. ** help: Help command
         5. ** fullwrite: Write value at all LBAs / usage: fullwrite <VALUE>
         6. ** fullread: Read all LBAs / usage: fullread
-        7. ** testapp1: After fullwrite, Check If fullread value is correct according to the written value / usage: testapp1
-        8. ** testapp2: After overwrite, read the LBA value to make sure LBA value is correct according to overwritten value / usage: testapp2
         ****************************************
                 """
 
         print(f'{help_str}')
         return help_str
-
-    def exit(self) -> str:
-        pass
