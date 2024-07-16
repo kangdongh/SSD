66b117afb8fd5757ad5ce24388ef44bae0a197f2
junhyeok-park <parkerk4@naver.com>
2024-07-16 16:52:16 +0900

    [refactor] remove unnecessary file

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -8,7 +8,6 @@ from hardware.ssd_writer import SSDWriter
 TEST_DATA_FILE_PATH = './test_nand.txt'
 TEST_RESULT_FILE_PATH = './test_result.txt'
 INITIAL_DATA_VALUE = '0x00000000'
-SSD_PATH = os.path.join(os.path.abspath(__file__), '../../hardware/ssd.py')
 
 
 class TestSSD(TestCase):
