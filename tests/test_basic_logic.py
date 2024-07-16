672319fba0d8750764d8008b6a9ceb74976c476a
jeonghun <jhe.kim@samsung.com>
2024-07-16 10:03:29 +0900

    [feat] Add to test basic logic

--- tests/test_basic_logic.py
+++ tests/test_basic_logic.py
@@ -1,27 +1,84 @@
-from unittest import TestCase, skip
+import warnings
+warnings.filterwarnings('ignore')
+
+import sys
+import io
+
+import unittest
+from unittest.mock import Mock
+
+from app.basic_logic import BasicLogic
+
+FULL_COUNT = 100
+LBA = 3
+VALUE = 0x00000000
 
 
-class TestBasicLogic(TestCase):
-    def test_read(self):
-        pass
+class TestBasicLogic(unittest.TestCase):
+    def setUp(self):
+        self.sut = BasicLogic("basic_logic/test")
+        self.sut = Mock()
+        self.output = io.StringIO()
 
-    def test_write(self):
-        pass
+    @unittest.skip
+    def test_read_pass(self):
+        sys.stdout = self.output
+        self.sut.read(LBA)
 
+        self.assertEqual(self.output.getvalue(), "PASS to read from " + str(LBA))
+
+    @unittest.skip
+    def test_read_wrong_address(self):
+        wrong_lba = -1
+
+        with self.assertRaises(Exception):
+            self.sut.read(wrong_lba)
+
+    @unittest.skip
+    def test_write_pass(self):
+        self.sut.write(LBA, VALUE)
+        # self.sut.read.return_value = VALUE
+
+        self.assertEqual(self.sut.read(LBA), VALUE)
+
+    @unittest.skip
+    def test_write_wrong_address(self):
+        wrong_lba = -1
+
+        with self.assertRaises(Exception):
+            self.sut.write(wrong_lba, VALUE)
+
+    @unittest.skip
+    def test_write_wrong_value(self):
+        wrong_value = -1
+
+        with self.assertRaises(Exception):
+            self.sut.write(LBA, wrong_value)
+
+    @unittest.skip
     def test_full_read(self):
-        pass
+        self.sut.full_read()
 
+        self.assertEqual(self.sut.read.call_count, FULL_COUNT)
+
+    @unittest.skip
     def test_full_write(self):
-        pass
+        self.sut.full_write(VALUE)
 
-    def test_exit(self):
-        pass
+        self.assertEqual(self.sut.write.call_count, FULL_COUNT)
 
-    def test_help(self):
-        pass
+    @unittest.skip
+    def test_exit_called_once(self):
+        self.sut.exit()
 
-    def test_test_app1(self):
-        pass
+        self.sut.exit.assert_called_once()
 
-    def test_test_app2(self):
-        pass
+    @unittest.skip
+    def test_help_called_once(self):
+        self.sut.help()
+
+        self.sut.help.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
