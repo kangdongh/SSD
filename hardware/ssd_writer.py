d01eebd488c8e9a3b85fb3feecd3b237fd27e371
junhyeok-park <parkerk4@naver.com>
2024-07-16 11:52:03 +0900

    [fix] add max_lba arguments in SSDWriter

--- hardware/ssd_writer.py
+++ hardware/ssd_writer.py
@@ -2,13 +2,13 @@ from hardware.ssd_writer_interface import ISSDWriter
 
 
 class SSDWriter(ISSDWriter):
-    def write(self, write_file_path: str, logical_bytes_address: int, data_to_write: str):
+    def write(self, write_file_path: str, logical_bytes_address: int, data_to_write: str, max_lba : int = 100):
         try:
-            if not self._is_valid_address_and_data(logical_bytes_address, data_to_write):
+            if not self._is_valid_address_and_data(logical_bytes_address, data_to_write, max_lba):
                 raise Exception('INVALID COMMAND')
             with open(write_file_path, 'r') as file:
                 lines = file.readlines()
-            if not self._is_valid_file(lines):
+            if not self._is_valid_file(lines, max_lba):
                 raise Exception('INVALID COMMAND')
 
             lines[logical_bytes_address] = data_to_write + '\n'
@@ -18,8 +18,8 @@ class SSDWriter(ISSDWriter):
         except Exception:
             raise Exception('INVALID COMMAND')
 
-    def _is_valid_address_and_data(self, address, data):
-        if address < 0 or address > 99 or len(data) != 10:
+    def _is_valid_address_and_data(self, address, data, max_lba):
+        if address < 0 or address > max_lba or len(data) != 10:
             return False
 
         if not data.startswith('0x'):
@@ -31,7 +31,7 @@ class SSDWriter(ISSDWriter):
                 return False
         return True
 
-    def _is_valid_file(self, file_line_lists):
-        if len(file_line_lists) != 100:
+    def _is_valid_file(self, file_line_lists, max_lba):
+        if len(file_line_lists) != max_lba:
             return False
         return True
