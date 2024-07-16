95ed46953b3565622380187e709257fe6dd56729
junhyeok-park <parkerk4@naver.com>
2024-07-16 13:51:02 +0900

    [fix] modify ssd_write function interface

--- hardware/ssd_writer_interface.py
+++ hardware/ssd_writer_interface.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class ISSDWriter(ABC):
     @abstractmethod
-    def write(self, write_file_name: str, logical_bytes_address: int, data_to_write: str):
+    def write(self, write_file_name: str, logical_bytes_address: int, data_to_write: str, max_lba: int = 100):
         pass
