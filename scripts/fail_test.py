c3fdb4f5930a1971812a3736df7bdc18f0b5fb1f
jeff.kang <jeff.kang@samsung.com>
2024-07-17 14:38:35 +0900

    [feat] clean up the script

--- scripts/fail_test.py
+++ scripts/fail_test.py
@@ -1,14 +1,6 @@
 import sys
 
-from app.api import write_ssd, read_ssd
 from app.system_call_handler import initialize_system_call_handler
-
-FIRST_WRITE_VALUE = '0xAAAABBBB'
-OVERWRITE_VALUE = '0x12345678'
-PREDEFINED_LBA_HEAD = 0
-PREDEFINED_LBA_TAIL = 6
-PREDEFINED_LBA_RANGE = range(PREDEFINED_LBA_HEAD, PREDEFINED_LBA_TAIL)
-DUPL_WRITE_COUNT = 10
 
 
 def fail_test():
