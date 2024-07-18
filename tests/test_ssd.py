24d00694c15924c3ffb99b7e6677b463e2238ba4
junhyeok-park <parkerk4@naver.com>
2024-07-18 13:39:48 +0900

    [feat] add tests for optimized buffer

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -1,20 +1,22 @@
 import os
-from unittest import TestCase, skip
+from unittest import TestCase
 
 from hardware.ssd import SSD
 
 TEST_DATA_FILE_PATH = './test_nand.txt'
 TEST_RESULT_FILE_PATH = './test_result.txt'
+TEST_BUFFER_FILE_PATH = './test_buffer.txt'
 INITIAL_DATA_VALUE = '0x00000000'
 
 
 class TestSSD(TestCase):
     def setUp(self):
-        self.ssd = SSD(TEST_DATA_FILE_PATH, TEST_RESULT_FILE_PATH)
+        self.ssd = SSD(TEST_DATA_FILE_PATH, TEST_RESULT_FILE_PATH, TEST_BUFFER_FILE_PATH)
 
     def tearDown(self):
         os.remove(TEST_DATA_FILE_PATH)
         os.remove(TEST_RESULT_FILE_PATH)
+        os.remove(TEST_BUFFER_FILE_PATH)
 
     def get_lines(self, file_name):
         with open(file_name, 'r') as f:
@@ -108,3 +110,66 @@ class TestSSD(TestCase):
             self.assertEqual('0x00000000', lines[2])
         with self.subTest("LBA=3"):
             self.assertEqual('0x000000DD', lines[3])
+
+    def test_optimize_duplicated_write(self):
+        self.ssd.run(['ssd', 'W', '20', '0xABCDABCD'])
+        self.ssd.run(['ssd', 'W', '21', '0x12341234'])
+        self.ssd.run(['ssd', 'W', '20', '0xEEEEFFFF'])
+        del self.ssd
+        lines = self.get_lines(TEST_BUFFER_FILE_PATH)
+        lines = [e for e in lines if e != 'None']
+        with self.subTest("BUFFER_LEN=2"):
+            self.assertEqual(2, len(lines))
+        with self.subTest("ADDR=21"):
+            self.assertEqual('W 21 0x12341234', lines[0])
+        with self.subTest("ADDR=20"):
+            self.assertEqual('W 20 0xEEEEFFFF', lines[1])
+
+    def test_optimize_post_erase(self):
+        self.ssd.run(['ssd', 'W', '20', '0xABCDABCD'])
+        self.ssd.run(['ssd', 'W', '21', '0x12341234'])
+        self.ssd.run(['ssd', 'E', '18', '5'])
+        del self.ssd
+        lines = self.get_lines(TEST_BUFFER_FILE_PATH)
+        lines = [e for e in lines if e != 'None']
+        with self.subTest("BUFFER_LEN=1"):
+            self.assertEqual(1, len(lines))
+        with self.subTest("ADDR=18"):
+            self.assertEqual('E 18 5', lines[0])
+
+    def test_optimize_merge_erase(self):
+        self.ssd.run(['ssd', 'W', '20', '0xABCDABCD'])
+        self.ssd.run(['ssd', 'E', '10', '2'])
+        self.ssd.run(['ssd', 'E', '12', '3'])
+        del self.ssd
+        lines = self.get_lines(TEST_BUFFER_FILE_PATH)
+        lines = [e for e in lines if e != 'None']
+        with self.subTest("BUFFER_LEN=2"):
+            self.assertEqual(2, len(lines))
+        with self.subTest("ADDR=20"):
+            self.assertEqual('W 20 0xABCDABCD', lines[0])
+        with self.subTest("ADDR=10"):
+            self.assertEqual('E 10 5', lines[1])
+
+    def test_optimize_narrow_range_of_erase(self):
+        self.ssd.run(['ssd', 'E', '10', '4'])
+        self.ssd.run(['ssd', 'E', '40', '5'])
+        self.ssd.run(['ssd', 'W', '12', '0xABCD1234'])
+        self.ssd.run(['ssd', 'E', '50', '1'])
+        self.ssd.run(['ssd', 'W', '13', '0x4BCD5351'])
+        self.ssd.run(['ssd', 'W', '50', '0xABCD1234'])
+        del self.ssd
+        lines = self.get_lines(TEST_BUFFER_FILE_PATH)
+        lines = [e for e in lines if e != 'None']
+        with self.subTest("BUFFER_LEN=5"):
+            self.assertEqual(5, len(lines))
+        with self.subTest("LINE 1"):
+            self.assertEqual('E 10 2', lines[0])
+        with self.subTest("LINE 2"):
+            self.assertEqual('E 40 5', lines[1])
+        with self.subTest("LINE 3"):
+            self.assertEqual('W 12 0xABCD1234', lines[2])
+        with self.subTest("LINE 4"):
+            self.assertEqual('W 13 0x4BCD5351', lines[3])
+        with self.subTest("LINE 5"):
+            self.assertEqual('W 50 0xABCD1234', lines[4])
