35856290908ecf4c338ab26f2c1968dcdf3ec4bb
junhyeok-park <parkerk4@naver.com>
2024-07-18 14:13:24 +0900

    [fix/refactor] fix bugs and refactor codes

--- hardware/buffer_optimizer.py
+++ hardware/buffer_optimizer.py
@@ -9,16 +9,16 @@ class BufferOptimizer:
         optimized_buffer = copy.copy(buffer)
         for i in range(size - 1):
             cur_idx = size - 1 - i
-            cur_cmd = optimized_buffer[size - 1 - i]
-            self._optimize_if_prev_command_editable(cur_cmd, cur_idx, optimized_buffer)
-            self._optimize_if_prev_command_useless(cur_cmd, cur_idx, optimized_buffer)
-            self._optimize_if_prev_command_mergable(cur_cmd, cur_idx, optimized_buffer)
+            self._optimize_if_prev_command_editable(cur_idx, optimized_buffer)
+            self._optimize_if_prev_command_useless(cur_idx, optimized_buffer)
+            self._optimize_if_prev_command_mergable(cur_idx, optimized_buffer)
         optimized_buffer = [e for e in optimized_buffer if e is not None]
         return optimized_buffer
 
-    def _optimize_if_prev_command_mergable(self, cur_cmd: List[str], cur_idx: int, command_buffer: List):
+    def _optimize_if_prev_command_mergable(self, cur_idx: int, command_buffer: List):
+        cur_cmd = command_buffer[cur_idx]
         prev_cmd = command_buffer[cur_idx - 1]
-        if cur_cmd is None or prev_cmd is None:
+        if (cur_cmd is None) or (prev_cmd is None):
             return
         cur_cmd_type = cur_cmd[0]
         prev_cmd_type = prev_cmd[0]
@@ -36,7 +36,8 @@ class BufferOptimizer:
             command_buffer[cur_idx - 1] = ['E', str(cur_start_addr), str(prev_end_addr - cur_start_addr)]
             command_buffer[cur_idx] = None
 
-    def _optimize_if_prev_command_useless(self, cur_cmd: List[str], cur_idx: int, command_buffer: List):
+    def _optimize_if_prev_command_useless(self, cur_idx: int, command_buffer: List):
+        cur_cmd = command_buffer[cur_idx]
         if cur_cmd is None:
             return
         for i in range(cur_idx):
@@ -46,7 +47,8 @@ class BufferOptimizer:
             if self._check_erasable(cur_cmd, prev_cmd):
                 command_buffer[i] = None
 
-    def _optimize_if_prev_command_editable(self, cur_cmd: List[str], cur_idx: int, command_buffer: List):
+    def _optimize_if_prev_command_editable(self, cur_idx: int, command_buffer: List):
+        cur_cmd = command_buffer[cur_idx]
         if cur_cmd is None:
             return
         for i in range(cur_idx):
@@ -54,9 +56,13 @@ class BufferOptimizer:
             if (prev_cmd is None) or (prev_cmd[0] != CMD_ERASE_TYPE) or (cur_cmd[0] != CMD_WRITE_TYPE):
                 continue
             cur_cmd_addr = int(cur_cmd[1])
-            prev_end_addr = int(prev_cmd[1]) + int(prev_cmd[2])
-            if cur_cmd_addr == (prev_end_addr - 1):
-                prev_cmd[2] = str(int(prev_cmd[2]) - 1)
+            prev_cmd_addr = int(prev_cmd[1])
+            prev_erase_size = int(prev_cmd[2])
+            if cur_cmd_addr == prev_cmd_addr and prev_erase_size > 1:
+                prev_cmd[1] = str(prev_cmd_addr + 1)
+                prev_cmd[2] = str(prev_erase_size - 1)
+            elif cur_cmd_addr == (prev_cmd_addr + prev_erase_size - 1):
+                prev_cmd[2] = str(prev_erase_size - 1)
 
     def _check_erasable(self, cur_cmd: List[str], prev_cmd: List[str]):
         cur_cmd_type = cur_cmd[0]
@@ -81,7 +87,7 @@ class BufferOptimizer:
         return prev_cmd_addr == cur_cmd_addr
 
     def _check_erasable_when_cur_write_prev_erase(self, cur_cmd_addr, prev_cmd_addr, prev_erase_size):
-        return cur_cmd_addr <= prev_cmd_addr and prev_erase_size == 1
+        return cur_cmd_addr == prev_cmd_addr and prev_erase_size <= 1
 
     def _check_erasable_when_cur_erase_prev_write(self, cur_cmd_addr, cur_erase_size, prev_cmd_addr):
         return cur_cmd_addr <= prev_cmd_addr < cur_cmd_addr + cur_erase_size
